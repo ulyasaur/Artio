76f7f5b9c7e394742b7a650cad5bbd39de375bd0
ulyasaur <ulyano4ka2003@gmail.com>
2023-02-02 12:12:24 +0200

    created interfaces

---
+++ Artio/DAL/Abstractions/ILikeRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace DAL.Abstractions
+{
+    public interface ILikeRepository
+    {
+        Task<List<Like>> GetAllLikes(Expression<Func<Like, bool>> filter);
+
+        Task AddLike(Like like);
+
+        Task DeleteLike(string userId, int postId);
+    }
+}
---
+++ Artio/DAL/Abstractions/ITagRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace DAL.Abstractions
+{
+    public interface ITagRepository
+    {
+        Task<List<Tag>> GetAllTagsAsync(Expression<Func<Tag, bool>> filter);
+
+        Task<Tag> GetTagAsync(Expression<Func<Tag, bool>> filter);
+
+        Task AddTagAsync(Tag tag);
+    }
+}
