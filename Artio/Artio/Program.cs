ec7d6f1c2329aabbaf348c7d39a8b08b7936477d
ulyasaur <ulyano4ka2003@gmail.com>
2023-02-20 22:09:40 +0200

    added seeds

--- Artio/Artio/Program.cs
+++ Artio/Artio/Program.cs
@@ -84,4 +84,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<ApplicationContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context, userManager);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration.");
+}
+
 app.Run();
